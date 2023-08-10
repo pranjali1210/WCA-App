27e584e79b05bf8a6c09c44b7fdcd3af33d22af5
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-08-10 15:17:01 +0530

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { init as initApm } from "@elastic/apm-rum";
+
+var apm = initApm({
+  serviceName: 'my-service-name',
+
+  serverUrl: 'https://6687db2a719043c78ac20f7343d2eb11.apm.us-central1.gcp.cloud.es.io:443',
+
+  serviceVersion: '',
+
+  environment: 'my-environment'
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
